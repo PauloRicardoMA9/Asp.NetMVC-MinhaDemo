5edc2703a88e526b470c44db9439afc1668c1454
Paulo Ricardo <paulo.silva@ma9.com.br>
2021-05-28 17:57:45 -0300

    Criação das views e controller de filme utilizando scaffold

---
+++ Data/MinhaDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MinhaDemo.Models;
+
+namespace MinhaDemo.Data
+{
+    public class MinhaDemoContext : DbContext
+    {
+        public MinhaDemoContext (DbContextOptions<MinhaDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Filme> Filme { get; set; }
+    }
+}
