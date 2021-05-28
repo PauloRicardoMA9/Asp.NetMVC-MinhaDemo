0a9cd96e6bc26af6f02e31a9c94ff6a20054afe5
Paulo Ricardo <paulo.silva@ma9.com.br>
2021-05-28 16:40:16 -0300

    Criação do formulário para adição de filmes

---
+++ Controllers/FilmesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinhaDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaDemo.Controllers
+{
+    public class FilmesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Adicionar(Filme filme)
+        {
+            if (ModelState.IsValid)
+            {
+                //
+            }
+
+            return View(filme);
+        }
+    }
+}
--- Models/Filme.cs
+++ Models/Filme.cs
@@ -12,28 +12,30 @@ namespace MinhaDemo.Models
         [Key]
         public int Id { get; set; }
         
+        [Display(Name = "Título")]
         [Required(ErrorMessage = "O campo Título é obrigatório")]
         [StringLength(60, ErrorMessage = "O título deve conter no máximo 60 caracteres")]
-        [Display(Name = "Título")]
         public string Titulo { get; set; }
 
+        [Display(Name = "Data de Lançamento")]
         [Required(ErrorMessage = "O campo Data de Lançamento é obrigatório")]
         [DataType(DataType.Date, ErrorMessage = "Data em formato incorreto")]
-        [Display(Name = "Data de Lançamento")]
         public DateTime DataLancamento { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF""'\w-]*$", ErrorMessage = "Gênero em formato inválido")]   // Primeira letra deve ser maiúscula, e as outras podem ser maiúsculas, minúsculas ou acentuações
-        [StringLength(30, ErrorMessage = "O gênero deve conter no máximo 30 caracteres")]
         [Display(Name = "Gênero")]
+        [Required(ErrorMessage = "O campo Gênero é obrigatório")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF""'\w-]*$", ErrorMessage = "Gênero em formato inválido, a primeira letra deve ser maiúscula")]   // Primeira letra deve ser maiúscula, e as outras podem ser maiúsculas, minúsculas ou acentuações
+        [StringLength(30, ErrorMessage = "O gênero deve conter no máximo 30 caracteres")]
         public string Genero { get; set; }
 
-        [Range(1, 1000, ErrorMessage = "O valor deve estrar entre 1 e 1000")]
         [Required(ErrorMessage = "O campo Valor é obrigatório")]
+        [Range(1, 1000, ErrorMessage = "O valor deve estrar entre 1 e 1000")]
         [Column(TypeName = "decimal(18,2)")]
         public decimal Valor { get; set; }
 
-        [RegularExpression(@"^[0-5]*$", ErrorMessage = "Avaliação em formato inválido")]    // Apenas números de 0 a 5
         [Display(Name = "Avaliação")]
+        [Required(ErrorMessage = "O campo Avaliação é obrigatório")]
+        [RegularExpression(@"^[0-5]*$", ErrorMessage = "Avaliação deve estar entre 0 e 5")]    // Apenas números de 0 a 5
         public int Avaliacao { get; set; }
         
     }
